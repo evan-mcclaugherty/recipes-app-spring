8f55e8ebcb0bd432c0a9e13dd434f3351316a563
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-11-16 22:11:31 -0700

    Add Project Lombok and Refactor domain models.

--- src/main/java/com/evan/recipes/domain/Category.java
+++ src/main/java/com/evan/recipes/domain/Category.java
@@ -1,8 +1,11 @@
 package com.evan.recipes.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 public class Category {
     @Id
@@ -12,27 +15,4 @@ public class Category {
     private Set<Recipe> recipes;
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
